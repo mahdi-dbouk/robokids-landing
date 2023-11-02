49003c9df9eb59bc9d00feb7279e19d13ddf2e2e
Mahdi Dbouk <mahdi.dbouk97@gmail.com>
2023-11-02 23:08:38 +0200

    [src/carousel.ts] Implemented Logic for Carousel Indicators

--- src/carousel.ts
+++ src/carousel.ts
@@ -2,11 +2,22 @@ const firstCard: HTMLDivElement = document.querySelector('#first-card')!
 const secondCard: HTMLDivElement = document.querySelector('#second-card')!
 const thirdCard: HTMLDivElement = document.querySelector('#third-card')!
 
+const firstIndicator: HTMLDivElement = document.querySelector('#first-indicator')!
+const secondIndicator: HTMLDivElement = document.querySelector('#second-indicator')!
+const thirdIndicator: HTMLDivElement = document.querySelector('#third-indicator')!
+
 const carouselHandler = () => {
     
     firstCard.classList.toggle('visible')
     secondCard.classList.toggle('hidden')
     thirdCard.classList.toggle('hidden')
+
+    firstIndicator.classList.add('bg-background')
+    firstIndicator.classList.remove('bg-white')
+    secondIndicator.classList.remove('bg-background')
+    secondIndicator.classList.add('bg-white')
+    thirdIndicator.classList.remove('bg-background')
+    thirdIndicator.classList.add('bg-white')
     
     let currentCardIndex = 0
     
@@ -23,6 +34,15 @@ const carouselHandler = () => {
         secondCard.classList.add('hidden')
         thirdCard.classList.remove('visible')
         thirdCard.classList.add('hidden')
+
+        firstIndicator.classList.add('bg-background')
+        firstIndicator.classList.remove('bg-white')
+        secondIndicator.classList.remove('bg-background')
+        secondIndicator.classList.add('bg-white')
+        thirdIndicator.classList.remove('bg-background')
+        thirdIndicator.classList.add('bg-white')
+
+
         
       }
       else if(currentCardIndex == 1) {
@@ -32,6 +52,13 @@ const carouselHandler = () => {
         secondCard.classList.remove('hidden')
         thirdCard.classList.remove('visible')
         thirdCard.classList.add('hidden')
+
+        firstIndicator.classList.remove('bg-background')
+        firstIndicator.classList.add('bg-white')
+        secondIndicator.classList.add('bg-background')
+        secondIndicator.classList.remove('bg-white')
+        thirdIndicator.classList.remove('bg-background')
+        thirdIndicator.classList.add('bg-white')
       }
       else if(currentCardIndex == 2){
         firstCard.classList.remove('visible')
@@ -40,6 +67,13 @@ const carouselHandler = () => {
         secondCard.classList.add('hidden')
         thirdCard.classList.add('visible')
         thirdCard.classList.remove('hidden')
+
+        firstIndicator.classList.remove('bg-background')
+        firstIndicator.classList.add('bg-white')
+        secondIndicator.classList.remove('bg-background')
+        secondIndicator.classList.add('bg-white')
+        thirdIndicator.classList.add('bg-background')
+        thirdIndicator.classList.remove('bg-white')
       }
     }
     const prevClicked = () => {
